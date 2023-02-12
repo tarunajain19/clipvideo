f8f66b81ebdfb7d03abeb7e37fd2aa4247feee73
Taruna jain <87579991+tarunajain19@users.noreply.github.com>
2023-02-13 00:33:41 +0530

    Update mashup.py

--- mashup.py
+++ mashup.py
@@ -53,7 +53,7 @@ def download_files(x,n):
     for i in range(n):
         yt = YouTube("https://www.youtube.com/watch?v=" + video_ids[i]) 
         print("Songs are downloading "+str(i+1)+" .......")
-#         mp4files = yt.streams.filter(only_audio=True).first().download(filename='song-'+str(i)+'.mp3')
+        mp4files = yt.streams.filter(only_audio=True).first().download(filename='song-'+str(i)+'.mp3')
 # def merge_sound(n,y):
 #     if os.path.isfile("song-0.mp3"):
 #         try:
